495c7c7b3791b84d63318ba6051f26db749abc20
tamarans7171 <tamarans7171@gmail.com>
2024-06-26 11:43:07 +0300

    work

--- db/mongooseconnect.js
+++ db/mongooseconnect.js
@@ -5,7 +5,6 @@ main().catch(err => console.log(err));
 async function main() {
 
   await mongoose.connect(`mongodb+srv://${config.userDB}:${config.passDB}@cluster0.afogddl.mongodb.net/dress4u`);
-  // await mongoose.connect(`mongodb://localhost:27017`);
   console.log("mongo connect mongo atlas");
 
 }
